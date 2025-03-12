e60013da529ad83269261567b2bab40327af2287
Roman Myachin <roma.mellon@mail.ru>
2025-03-12 21:30:49 +0500

    add func tests and README

--- tests/test_classess.py
+++ tests/test_classess.py
@@ -1,27 +1,36 @@
-import pytest
-from src.classess import Product, Category
+from src.classess import Product
 
 
-@pytest.fixture
-def product_apple():
-    return Product("Apple", "red", 99.9, 1000)
+def test_category_init(first_category, second_category):
+    assert first_category.name == "Смартфоны"
+    assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert len(first_category.products_in_list) == 3
+
+    assert first_category.category_count == 2
+    assert second_category.product_count == 4
 
 
-def test_init_product(product_apple):
-    assert product_apple.name == "Apple"
-    assert product_apple.description == "red"
-    assert product_apple.price == 99.9
-    assert product_apple.quantity == 1000
+def test_category_products_property(first_category):
+    assert (first_category.products == '\nSamsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n'
+                                       'Iphone 15, 210000.0 руб. Остаток: 8 шт.\n'
+                                       'Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.')
 
 
-@pytest.fixture
-def category_fruit():
-    return Category("fruits", "fruits from India", ["banana", "mango"])
 
+def test_product(product):
+    assert product.name == "55\" QLED 4K"
+    assert product.description == "Фоновая подсветка"
+    assert product.price == 123000.0
+    assert product.quantity == 7
 
-def test_init_category(category_fruit):
-    assert category_fruit.name == "fruits"
-    assert category_fruit.description == "fruits from India"
-    assert category_fruit.products == ["banana", "mango"]
-    assert category_fruit.product_count == 2
-    assert Category.category_count == 1
+def test_new_product():
+    product = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    product.name = "55\" QLED 4K"
+    product.description = "Фоновая подсветка"
+    product.price = 123000.0
+    product.quantity = 7
+
+def test_price_update(capsys, product):
+    product.price = -100
+    message = capsys.readouterr()
+    assert message.out.strip() == 'Цена не должна быть нулевая или отрицательная'
