b90351baa71ef88021b4a8b2d507d9d1d1c2fb7c
Roman Myachin <roma.mellon@mail.ru>
2025-03-18 11:35:30 +0500

    add func

--- src/classess.py
+++ src/classess.py
@@ -14,7 +14,9 @@ class Product:
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
     def __add__(self, other):
-        return self.price * self.quantity + other.price * other.quantity
+        if type(other) is Product:
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError
 
     @property
     def price(self):
@@ -50,8 +52,11 @@ class Category:
         return f'{self.name}, колличество продуктов: {Category.product_count} шт.'
 
     def add_product(self, product: Product):
-        self.__list_product.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__list_product.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self):
